248cf979c3f22d3192229ea9173f32522c3ecfc3
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-01 14:20:55 +0200

    add controller and form

---
+++ src/Controller/Admin/SetMenusController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\SetMenu;
+use App\Form\SetMenuFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/formules', name: 'app_setmenus_')]
+class SetMenusController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+
+
+
+        return $this->render('admin/setmenus/index.html.twig', [
+            'controller_name' => 'SetMenusController',
+        ]);
+    }
+
+    #[Route('/ajout', name: 'add')]
+    public function add(        
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+
+        $setmenu = new SetMenu();
+
+        $setmenu_form = $this->createForm(SetMenuFormType::class, $setmenu);
+
+        $setmenu_form->handleRequest($request);
+
+        if ($setmenu_form->isSubmitted() && $setmenu_form->isValid()) {
+
+            $entityManager->persist($setmenu);
+            $entityManager->flush();
+
+        }
+
+        return $this->render('admin/setmenus/add.html.twig', [
+            'setmenuForm' => $setmenu_form->createView(),
+        ]);
+    }
+
+    #[Route('/edition/{id}', name: 'edit')]
+    public function edit(
+        SetMenu $setmenu,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+
+        $setmenu_form = $this->createForm(SetMenuFormType::class, $setmenu);
+
+        $setmenu_form->handleRequest($request);
+
+        if ($setmenu_form->isSubmitted() && $setmenu_form->isValid()) {
+
+            $entityManager->persist($setmenu);
+            $entityManager->flush();
+
+        }
+        return $this->render('setmenus/index.html.twig', [
+            'setmenuForm' => $setmenu_form->createView(),
+        ]);
+    }
+
+    #[Route('/suppression/{id}', name: 'delete')]
+    public function delete(): Response
+    {
+        return $this->render('setmenus/index.html.twig', [
+            'controller_name' => 'SetMenusController',
+        ]);
+    }
+}
---
+++ src/Form/SetMenuFormType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CourseCategory;
+use App\Entity\SetMenu;
+use App\Repository\CourseCategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Positive;
+
+class SetMenuFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', options:[
+                'label' => 'Nom de la Formule'
+            ])
+            ->add('summary', options:[
+                'label' => 'Description de la Formule'
+            ])
+            ->add('price', MoneyType::class, options:[
+                'label' => 'Prix',
+                'divisor' => 100,
+                'constraints' => [
+                    new Positive(
+                        message: 'Prix non valide'
+                    )
+                    ],
+            ])
+            ->add('courseCategory', EntityType::class, [
+                'class' => CourseCategory::class, 
+                'choice_label' => 'label',
+                'label' => 'Catégories de Plats acceptées',
+                'multiple' => true,
+                'expanded' => false,
+                'query_builder' => function(CourseCategoryRepository $ccr) {
+                    return $ccr->createQueryBuilder('c')
+                        ->orderBy('c.label', 'ASC');
+                },
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SetMenu::class,
+        ]);
+    }
+}
