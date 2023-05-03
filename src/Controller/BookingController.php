2131ab6ea94ea4dc71ae94256f1cc181e2709682
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-03 10:37:50 +0200

    add autocompletion for connected user in booking form

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Booking;
+use App\Entity\Customer;
 use App\Form\BookingFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,12 +20,42 @@ class BookingController extends AbstractController
     ): Response
     {
 
-
         $booking = new Booking();
 
         $booking_form = $this->createForm(BookingFormType::class, $booking);
 
         $booking_form->handleRequest($request);
+
+        // On récupère l'entité User connecté ou false si 
+        // l'utilisateur n'est pas connecté
+        //
+        // L'annotation @var $user User permet à Symfony
+        // de savoir que $user est de type UserInterface
+        // et permet d'utiliser les méthodes accesseurs tel
+        // que getFirstname()
+        /** @var $user Customer */
+        $user = $this->getUser();
+
+        // On vérifie si l'utilisateur est connecté
+        // et que l'objet User et bien un Customer
+        // un Admin n'aura pas les mêmes méthodes et
+        // accesseurs.
+        if ($user && $user instanceof Customer){
+            $data = $booking_form->getData();
+
+            $data->setCustomerFirstname($user->getFirstname());
+            $data->setCustomerLastname($user->getLastname());
+            $data->setCustomerPhone($user->getPhone());
+            $data->setCustomerMail($user->getEmail());
+            $data->setCovers($user->getDefaultCovers());
+            $data->setCustomer($user);
+
+            foreach($user->getAllergens() as $allergen) {
+                $data->addAllergens($allergen);
+            }
+
+            $booking_form->setData($data);
+        } 
 
         if($booking_form->isSubmitted() && $booking_form->isValid()) {
 
