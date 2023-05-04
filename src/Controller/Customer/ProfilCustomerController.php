d602b85bfe3a51082cfc6a7ad0fbdc64edda9c43
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-04 14:40:59 +0200

    add customer information in profil page + can modify it

--- src/Controller/Customer/ProfilCustomerController.php
+++ src/Controller/Customer/ProfilCustomerController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Customer;
 
+use App\Form\ModifyCustomerInfoFormType;
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +14,9 @@ class ProfilCustomerController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil_customer')]
     public function index(
-        BookingRepository $bookingRepository
+        BookingRepository $bookingRepository,
+        Request $request,
+        EntityManagerInterface $entityManager
     ): Response
     {
         $this->denyAccessUnlessGranted('ROLE_CUSTOMER');
@@ -19,11 +24,26 @@ class ProfilCustomerController extends AbstractController
         /** @var $user Customer */
         $user = $this->getUser();
 
+        $user_form = $this->createForm(ModifyCustomerInfoFormType::class, $user);
+
+        $user_form->handleRequest($request);
+
+        if ($user_form->isSubmitted() && $user_form->isValid()) {
+
+            $this->addFlash('success', 'Informations modifiées avec succès');
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profil_customer');
+        }
+
         $user_books = $bookingRepository->findBy(['customer' => $user]);
 
         return $this->render('profil_customer/index.html.twig', [
             'user' => $user,
-            'books' => $user_books
+            'books' => $user_books,
+            'userForm' => $user_form
         ]);
     }
 }
--- src/Form/ModifyCustomerInfoFormType.php
+++ src/Form/ModifyCustomerInfoFormType.php
@@ -20,7 +20,7 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
-class RegistrationFormType extends AbstractType
+class ModifyCustomerInfoFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -60,31 +60,6 @@ class RegistrationFormType extends AbstractType
                     ->orderBy('a.label', 'ASC');
                 },
                 'required' => false
-            ])
-            ->add('RGPDConsent', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Il faut accepter les termes sur les conditions d\'utilisation.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
             ])
         ;
     }
