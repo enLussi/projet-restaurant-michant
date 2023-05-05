54c30dc583c925c1b08f5a5744655685d90a7264
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-05 15:35:30 +0200

    add gallery section and booking section in main page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(
+        PictureRepository $pictureRepository
+    ): Response
     {
+
+        $gallery = $pictureRepository->findAll();
+
+        if(count($gallery) <= 6) {
+            $last_picture = $gallery;
+        } else {
+            $last_picture = array_slice($gallery, count($gallery)-6);
+        }
+
+        $last_picture = array_reverse($last_picture);
+
+
         return $this->render('main/index.html.twig', [
+            'gallery' => $last_picture,
         ]);
     }
 }
