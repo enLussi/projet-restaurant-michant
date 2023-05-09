5901840335625c9d3f331509fd4bafc7c426c879
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-09 09:28:58 +0200

    fix data fixtures max booking default value

--- src/DataFixtures/HoursFixtures.php
+++ src/DataFixtures/HoursFixtures.php
@@ -11,30 +11,30 @@ class HoursFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $all_hours = [
-            ['Lundi', '00:00', '00:00', false, true, 'monday'],
-            ['Lundi', '00:00', '00:00', false, false, 'monday'],
-            ['Mardi', '00:00', '00:00', false, true, 'tuesday'],
-            ['Mardi', '18:30', '22:00', true, false, 'tuesday'],
-            ['Mercredi', '12:00', '14:00', true, true, 'wednesday'],
-            ['Mercredi', '18:30', '22:00', true, false, 'wednesday'],
-            ['Jeudi', '12:00', '14:00', true, true, 'thursday'],
-            ['Jeudi', '18:30', '22:00', true, false, 'thursday'],
-            ['Vendredi', '12:00', '14:00', true, true, 'friday'],
-            ['Vendredi', '18:30', '22:00', true, false, 'friday'],
-            ['Samedi', '11:20', '14:30', true, true, 'saturday'],
-            ['Samedi', '18:30', '22:30', true, false, 'saturday'],
-            ['Dimanche', '11:20', '14:30', true, true, 'sunday'],
-            ['Dimanche', '00:00', '00:00', false, false, 'sunday']
+            ['Lundi', '00:00', '00:00', false, true, 'monday', 5],
+            ['Lundi', '00:00', '00:00', false, false, 'monday', 5],
+            ['Mardi', '00:00', '00:00', false, true, 'tuesday', 5],
+            ['Mardi', '18:30', '22:00', true, false, 'tuesday', 5],
+            ['Mercredi', '12:00', '14:00', true, true, 'wednesday', 5],
+            ['Mercredi', '18:30', '22:00', true, false, 'wednesday', 5],
+            ['Jeudi', '12:00', '14:00', true, true, 'thursday', 5],
+            ['Jeudi', '18:30', '22:00', true, false, 'thursday', 5],
+            ['Vendredi', '12:00', '14:00', true, true, 'friday', 5],
+            ['Vendredi', '18:30', '22:00', true, false, 'friday', 5],
+            ['Samedi', '11:20', '14:30', true, true, 'saturday', 5],
+            ['Samedi', '18:30', '22:30', true, false, 'saturday', 5],
+            ['Dimanche', '11:20', '14:30', true, true, 'sunday', 5],
+            ['Dimanche', '00:00', '00:00', false, false, 'sunday', 5]
         ];
 
         foreach( $all_hours as $hours) {
-            $manager->persist($this->addHours($hours[0], $hours[1], $hours[2], $hours[3], $hours[4], $hours[5]));
+            $manager->persist($this->addHours($hours[0], $hours[1], $hours[2], $hours[3], $hours[4], $hours[5], $hours[6]));
         }
 
         $manager->flush();
     }
 
-    private function addHours(string $day, string $opening, string $closure, bool $open, bool $lunch, string $label) : Hours{
+    private function addHours(string $day, string $opening, string $closure, bool $open, bool $lunch, string $label, int $max) : Hours{
         $hours = new Hours();
 
         $hours->setLabelDay($day);
@@ -43,6 +43,7 @@ class HoursFixtures extends Fixture
         $hours->setOpen($open);
         $hours->setLunch($lunch);
         $hours->setLabel($label);
+        $hours->setMaxBooking($max);
         
         return $hours;
     }
