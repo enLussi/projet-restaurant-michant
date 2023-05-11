07fe3e4244194286743cebac811e334bf2facde3
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-05-11 10:05:42 +0200

    fix bug with flash message and navbar js on reset password page

--- public/assets/js/responsiveNavbar.js
+++ public/assets/js/responsiveNavbar.js
@@ -2,14 +2,16 @@ const buttonNav = document.getElementById('navbar-collapser');
 
 const navigation = document.getElementById('navigation');
 
-buttonNav.onclick = () => {
+if(buttonNav !== null){
+  buttonNav.onclick = () => {
 
-  if(navigation.classList.contains('shown')) {
-    navigation.classList.remove('shown');
-    navigation.classList.add('hidden');
-  } else {
-    navigation.classList.remove('hidden');
-    navigation.classList.add('shown');
+    if(navigation.classList.contains('shown')) {
+      navigation.classList.remove('shown');
+      navigation.classList.add('hidden');
+    } else {
+      navigation.classList.remove('hidden');
+      navigation.classList.add('shown');
+    }
+    
   }
-  
 }
